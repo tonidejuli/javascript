a1f28fe09c1091c5c34ab238088035c3013588ac
tonidejuli <220539207+tonidejuli@users.noreply.github.com>
2025-12-01 20:20:26 -0300

    segundo exercicio

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,42 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id' , 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade > 0 && idade < 12) {
+                img.setAttribute('src' , 'menino.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src' , 'jovem-homem.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src' , 'homem.jpg')
+            } else {
+                img.setAttribute('src' , 'idoso.jpg')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade > 0 && idade < 12) {
+                img.setAttribute('src' , 'menina.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src' , 'jovem-mulher.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src' , 'mulher.jpg')
+            } else {
+                img.setAttribute('src' , 'idosa.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
+    
